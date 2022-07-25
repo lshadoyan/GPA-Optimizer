dd9830d7f8122a8952ccae7101f5324602cd02ea
lshadoyan <lshadoyan@gmail.com>
2022-07-24 23:43:09 -0400

    condensing

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import os
 import sys
 
-import pandas
+import pandas as pd
 import tabula
-import PyPDF2 
 
 file_path = r"C:\Users\Lucas\Desktop\PDF-Extract\Pathways Course Guide by Alpha.pdf"
 
-pages = tabula.read_pdf(file_path, stream=True, pages="all")
-page_num = len(pages)
+#pages = tabula.read_pdf(file_path, stream=True, pages="all")
+#page_num = len(pages)
 
-tables = tabula.read_pdf(file_path, pages="all",multiple_tables=True)
+#df = tabula.read_pdf(file_path, pages="all", lattice=True, multiple_tables=True)
 #for page in range(page_num):
 #    tables[page].to_excel('test.xlsx', index=False)
-table = table.concat(tables) 
-table.to_excel('test.xlsx', index=False)
+#table = table.concat(tables) 
+#df[0].to_excel('test.xlsx', index=False)
+df = tabula.convert_into(file_path, "test.csv", output_format='csv', lattice=True, pages="all")
+
